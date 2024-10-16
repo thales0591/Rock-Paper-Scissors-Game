8595a9169148d69c00f08c30bf2a382418828976
thales0591 <thales0591@gmail.com>
2024-10-16 10:19:50 -0300

    create Header component and configure main-card

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,15 @@ function Header() {
 
     return (
         <nav className="text-center mt-6">
-            <h1>Header</h1>
-        </nav>
+            <h1 className="font-passion text-4xl text-sky-700">LET'S PLAY ROCK, PAPER AND SCISSORS!</h1>
+            <h1 className='text-4xl'>✊✋✌</h1>
+            <div className="flex justify-center w-full mt-2">
+                <div className="border-4 border-blue-300 rounded-xl w-1/2">
+                    <h1 className="font-passion font-thin text-4xl">SCORE:</h1>
+                    <h1 className='font-play font-bold text-7xl'>{playerScore}</h1>
+                </div>
+            </div>
+        </nav>
     )
 }
 
