36902d168301940cf781bf2db4cd3707afe3ddbe
thales0591 <thales0591@gmail.com>
2024-10-17 10:13:38 -0300

    :recycle: refactor: turn header into a responsive component to mobile-desktop

--- src/components/Main_Card.jsx
+++ src/components/Main_Card.jsx
@@ -1,7 +1,7 @@
 function Main_Card({ header, content }) {
     return (
         <div className="bg-blue-800 h-screen flex justify-center items-center ">
-            <div className=" bg-white w-10/12 h-[85%] rounded-2xl shadow-2xl flex-col items-center">
+            <div className=" bg-white w-10/12 h-[90%] rounded-2xl shadow-2xl flex flex-col items-center">
                 { header }
                 { content }
             </div>
