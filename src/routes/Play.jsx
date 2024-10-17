2b1ed53893e346c136c483148cdfffe565c78fba
thales0591 <thales0591@gmail.com>
2024-10-17 10:13:38 -0300

    :recycle: refactor: turn header a responsive component to mobile-desktop

--- src/routes/Play.jsx
+++ src/routes/Play.jsx
@@ -1,9 +1,14 @@
 import rock from "../assets/rock.png"
+import paper from "../assets/paper.png"
+import scissor from "../assets/scissor.png"
 
 function Play () {
     return (
-    <div className="text-center">
-        <h1>Play</h1>
+    <div className="text-center mt-2 h-4/6 w-full flex flex-col justify-around items-center">
+        <h1 className="font-passion text-sky-700 text-[38px]">Make your move!</h1>
+        <img src={rock} alt="rock hand image" className="hover:scale-110 transition-transform duration-0.3s" />
+        <img src={paper} alt="paper hand image" className="hover:scale-110 transition-transform duration-0.3s" />
+        <img src={scissor} alt="scissor hand image" className="hover:scale-110 transition-transform duration-0.3s" />
     </div>
     )
 }
