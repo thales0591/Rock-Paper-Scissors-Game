a2112053ee5298d10c4c8911e2aab731e3378ac1
thales0591 <thales0591@gmail.com>
2024-10-18 17:59:36 -0300

    :sparkles: feature: create initial game logic and players ways to engage

--- src/routes/Game.jsx
+++ src/routes/Game.jsx
@@ -1,6 +1,72 @@
+import { useLocation } from "react-router-dom";
+import bsrock from "../assets/bs-rock.png";
+import bspaper from "../assets/bs-paper.png";
+import bsscissor from "../assets/bs-scissor.png";
+
 function Game() {
+
+   const location = useLocation();
+   const {machine, player} = location.state || {};
+
+   function checkMachinePlay() {
+      if (machine == "rock") {
+         return bsrock
+      }
+      if (machine == "paper") {
+         return bspaper
+      }
+      if (machine == "scissor") {
+         return bsscissor  
+      }
+   }
+   
+   function gameResult() {
+      if (player == "rock") {
+         if (machine == "paper") {
+            return "perdeu"
+         }
+         if (machine == "scissor") {
+            return "ganhou"
+         }
+         return "empatou"
+      }
+      else if (player == "paper") {
+         if (machine == "rock") {
+            return "ganhou"
+         }
+         if (machine == "scissor") {
+            return "perdeu"
+         }
+         return "empatou"
+      }
+      else {
+         if (machine == "rock") {
+            return "perdeu"
+         }
+         if (machine == "paper") {
+            return "ganhou"
+         }
+         return "empatou"
+      }
+   }
+
+
    return(
-    <h1>Game</h1>    
+      location.state ? 
+      (<div> 
+         {player == "rock" && (
+            <img src={bsrock} alt="rock hand image" />)}
+         {player == "paper" && (
+            <img src={bspaper} alt="paper hand image" />)}
+         {player == "scissor" && (
+            <img src={bsscissor} alt="scissor hand image" />)}
+         <h1>{gameResult()}</h1>
+         <img src={checkMachinePlay()} alt="" />
+      </div>)
+         :
+      (<div className="flex items-center w-full h-full justify-center">
+         <h1 className="mb-48 w-[80%] font-passion text-center text-sky-700 text-[32px] lg:text-[50px]">Que pena! Você ainda não fez nenhuma jogada :(</h1>
+      </div>)
    ) 
 }
 export default Game
--- src/routes/Play.jsx
+++ src/routes/Play.jsx
@@ -4,24 +4,52 @@ import scissor from "../assets/scissor.png"
 import bsrock from "../assets/bs-rock.png"
 import bspaper from "../assets/bs-paper.png"
 import bsscissor from "../assets/bs-scissor.png"
+import { Link } from "react-router-dom"
 
-function Play () {
+
+function Play ({ pScore, pSetScore }) {
+
+    const moves = ['rock', 'paper', 'scissor'];
+
+    function machineMove () {
+        return moves[Math.floor(Math.random() * 3)]
+    }
 
     return (
     <>
     <div className=" w-full text-center">
         <h1 className="font-passion text-sky-700 text-[38px] lg:text-[50px]">Make your move!</h1>
     </div>
+
     <div className="text-center h-[32rem] w-full flex flex-col justify-around items-center lg:flex-row lg:hidden">
-        <img src={rock} alt="rock hand image" className="hover:scale-110 transition-transform duration-0.3s" />
-        <img src={paper} alt="paper hand image" className="hover:scale-110 transition-transform duration-0.3s" />
-        <img src={scissor} alt="scissor hand image" className="hover:scale-110 transition-transform duration-0.3s" />
-       
+
+        <Link to="/game" 
+        state={{player: "rock", machine: machineMove()}}>
+        <img src={rock} alt="rock hand image" className="hover:scale-110 transition-transform duration-0.3s" /></Link>
+
+        <Link to="/game"
+        state={{player: "paper", machine: machineMove()}}>
+        <img src={paper} alt="paper hand image" className="hover:scale-110 transition-transform duration-0.3s" /></Link>
+
+        <Link to="/game"
+        state={{player: "scissor", machine: machineMove()}}>
+        <img src={scissor} alt="scissor hand image" className="hover:scale-110 transition-transform duration-0.3s" /></Link>
+
     </div>
+
     <div className="text-center h-[32rem] w-full flex-col justify-around items-center lg:flex-row hidden lg:flex">
-        <img src={bsrock} alt="rock hand image" className="hover:scale-110 transition-transform duration-0.3s"/>
-        <img src={bspaper} alt="rock hand image" className="hover:scale-110 transition-transform duration-0.3s" />
-        <img src={bsscissor} alt="rock hand image" className="hover:scale-110 transition-transform duration-0.3s" />
+
+        <Link to="/game" 
+        state={{player: "rock", machine: machineMove()}}>
+        <img src={bsrock} alt="rock hand image" className="hover:scale-110 transition-transform duration-0.3s" /></Link>
+
+        <Link to="/game"
+         state={{player: "paper", machine: machineMove()}}>
+        <img src={bspaper} alt="rock hand image" className="hover:scale-110 transition-transform duration-0.3s" /></Link>
+
+        <Link to="/game"
+        state={{player: "scissor", machine: machineMove()}}>
+        <img src={bsscissor} alt="rock hand image" className="hover:scale-110 transition-transform duration-0.3s" /></Link>
     </div>
     </>
     )
